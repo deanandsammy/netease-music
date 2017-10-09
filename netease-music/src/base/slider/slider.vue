29d190bd339a43dcf25473854c9a912317347371
elegantspirit <hkissbigapple@163.com>
2017-10-09 22:16:32 +0800

    carrousel

---
+++ netease-music/src/base/slider/slider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- netease-music/src/components/recommend/recommend.vue
+++ netease-music/src/components/recommend/recommend.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>推荐页面</div>
+  <div class="recommend" ref="recommend">
+    <div class="slider-wrapper" ref="sliderWrapper">
+      <slider>
+        <div>
+          <a>
+            <img>
+          </a>
+        </div>
+      </slider>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
 
+  export default {
+    data() {
+      return {
+        recommends: [],
+        discList: []
+      }
+    },
+
+    components: {
+      Slider
+    }
+  }
 </script>
 
 <style rel="stylesheet/stylus" lang="stylus">
