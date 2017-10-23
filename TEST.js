c1fc646b552a9c7ecd9d5eb0dd8ff95f1d965ae9
Vic Hu <hkissbigapple@163.com>
2017-10-23 22:55:41 +0800

    Update TEST.js

--- TEST.js
+++ TEST.js
@@ -1,9 +1,13 @@
 let arr = [1, 3, 5];
 let len = arr.length;
 
+
+// array 倒序排列实现
 for (let i = 0; i < len / 2; i++) {
   let temp = arr[i];
   
   arr[i] = arr[len - 1 - i];
   arr[len - 1 - i] = temp;
 }
+
+arr.map()
