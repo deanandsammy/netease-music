9a01041c5ad89587bfe1c26c0177a7c2dd07d2db
elegantspirit <hkissbigapple@163.com>
2017-11-21 17:03:16 +0800

    music-list component

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,15 +1,22 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import {mapState} from 'vuex'
   import {getSingerDetail} from 'api/singer'
+  import {createSong} from 'common/js/song'
   import {ERR_OK} from 'api/config'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
+    data() {
+      return {
+        song: []
+      }
+    },
     computed: {
       ...mapState([
         'singer'
@@ -20,32 +27,40 @@
     },
     methods: {
       _getDetail() {
-        if (this.singer.id) {
+        if (!this.singer.id) {
           this.$router.push('/singer')
           return
         }
 
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list[0])
+            this.songs = this._normalizeSongs(res.data.list)
           }
         })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+
+        list.forEach((item) => {
+          let {musicData} = item
+
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+
+        return ret
       }
+    },
+
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all .3s
