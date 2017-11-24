5782be9b3b73edc10b10e991577155199d049c5a
elegantspirit <hkissbigapple@163.com>
2017-11-24 13:49:58 +0800

    update state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,12 @@
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playlist: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1,
+  disc: {}
 }
 
 export default state
